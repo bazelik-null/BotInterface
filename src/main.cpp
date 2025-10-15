b6375ba22854b543e8e2c3ea94da2eba6217da77
bazelik-null <discordktdr@gmail.com>
2025-10-16 00:18:55 +0300

    Remove themes

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,6 @@ int main(int argc, char* argv[]) {
     // Create GTK app
     const auto app = Gtk::Application::create("org.gtkmm.botInterface", Gio::Application::Flags::NONE);
 
-    // Create new CSS provider and set path to CSS file
-    const auto css_provider = Gtk::CssProvider::create();
-    const std::string css_file = "../resources/gtk.css";
-
-    // Load CSS theme
-    css_provider->load_from_path(css_file);
-
-    // Set CSS theme
-    Gtk::StyleContext::add_provider_for_display(
-        Gdk::Display::get_default(),
-        css_provider,
-        GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-
     // Create the main window
     MainWindow window;
 
